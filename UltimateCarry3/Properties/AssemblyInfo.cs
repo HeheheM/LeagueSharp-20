57bd64d457a0d9a06f13e48ab5188354916b5b82
HeheheM <kefirowaty123@gmail.com>
2015-04-12 22:47:56 +0200

    Update AssemblyInfo.cs

--- UltimateCarry3/Properties/AssemblyInfo.cs
+++ UltimateCarry3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UltimateCarry")]
+[assembly: AssemblyTitle("UltimateCarryHeheheM")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UltimateCarry")]
+[assembly: AssemblyProduct("UltimateCarryHeheheM")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
